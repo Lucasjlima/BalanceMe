16ee5aa3dc7ecf4007ab9c75e8a80d6e839543a4
Lucas <lucasjlima2006@gmail.com>
2025-11-17 11:58:20 -0300

    feat: adding Goal Mapper

---
+++ src/main/java/br/com/fiap/balanceMe/goal/mapper/GoalMapper.java
@@ -0,0 +1,42 @@
+package br.com.fiap.balanceMe.goal.mapper;
+
+import br.com.fiap.balanceMe.goal.dto.request.GoalRequest;
+import br.com.fiap.balanceMe.goal.dto.response.GoalResponse;
+import br.com.fiap.balanceMe.goal.entity.Goal;
+import lombok.experimental.UtilityClass;
+
+import java.time.LocalDateTime;
+
+@UtilityClass
+public class GoalMapper {
+
+    public static Goal toGoal(GoalRequest request) {
+        return Goal
+                .builder()
+                .user(request.user())
+                .category(request.category())
+                .frequency(request.frequency())
+                .unitMeasure(request.unitMeasure())
+                .createdAt(LocalDateTime.now())
+                .isActive(true)
+                .startDate(request.startDate())
+                .endDate(request.endDate())
+                .build();
+
+    }
+
+    public static GoalResponse toGoalResponse(Goal goal) {
+        return GoalResponse
+                .builder()
+                .id(goal.getId())
+                .user(goal.getUser())
+                .category(goal.getCategory())
+                .frequency(goal.getFrequency())
+                .unitMeasure(goal.getUnitMeasure())
+                .createdAt(goal.getCreatedAt())
+                .isActive(goal.getIsActive())
+                .startDate(goal.getStartDate())
+                .endDate(goal.getEndDate())
+                .build();
+    }
+}
