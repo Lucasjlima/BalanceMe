38c3368969e2f345a23c4cd6ebc80f1b36dc0b51
Lucas <lucasjlima2006@gmail.com>
2025-11-17 11:52:12 -0300

    fix: adding filterchain to disable authorize request

--- src/main/java/br/com/fiap/balanceMe/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/balanceMe/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package br.com.fiap.balanceMe.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,5 +14,12 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
     }
 }
