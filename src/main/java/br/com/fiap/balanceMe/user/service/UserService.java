746c1806fa926547c4f029c50acdfd57e767eb5d
Lucas <lucasjlima2006@gmail.com>
2025-11-19 09:28:10 -0300

    feat: user can be edited

--- src/main/java/br/com/fiap/balanceMe/user/service/UserService.java
+++ src/main/java/br/com/fiap/balanceMe/user/service/UserService.java
@@ -1,6 +1,7 @@
 package br.com.fiap.balanceMe.user.service;
 
 import br.com.fiap.balanceMe.config.SecurityConfig;
+import br.com.fiap.balanceMe.user.dto.request.UserUpdateRequest;
 import br.com.fiap.balanceMe.user.entity.User;
 import br.com.fiap.balanceMe.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +27,29 @@ public class UserService {
         user.setIsActive(true);
         user.setPassword(securityConfig.passwordEncoder().encode(user.getPassword()));
         return repository.save(user);
+    }
+
+    @Transactional
+    public Optional<User> edit(UserUpdateRequest request, Long userId) {
+        Optional<User> optUser = repository.findById(userId);
+        if(optUser.isPresent()) {
+            User user = optUser.get();
+
+            if(request.username() != null) {
+                user.setUsername(request.username());
+            }
+            if(request.userEmail() != null) {
+                user.setUserEmail(request.userEmail());
+            }
+            if(request.timezone() != null) {
+                user.setTimezone(request.timezone());
+            }
+            if(request.role() != null) {
+                user.setRole(request.role());
+            }
+            repository.save(user);
+            return Optional.of(user);
+        }
+        return Optional.empty();
     }
 }
