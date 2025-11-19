a1a24709b6eb1ae1570b3cd6463ee78a0764c0c4
Lucas <lucasjlima2006@gmail.com>
2025-11-19 09:34:52 -0300

    feat: functions implemented on goal controller

--- src/main/java/br/com/fiap/balanceMe/goal/controller/GoalController.java
+++ src/main/java/br/com/fiap/balanceMe/goal/controller/GoalController.java
@@ -1,7 +1,9 @@
 package br.com.fiap.balanceMe.goal.controller;
 
 import br.com.fiap.balanceMe.goal.dto.request.GoalRequest;
+import br.com.fiap.balanceMe.goal.dto.request.GoalUpdateRequest;
 import br.com.fiap.balanceMe.goal.dto.response.GoalResponse;
+import br.com.fiap.balanceMe.goal.dto.response.GoalUpdateResponse;
 import br.com.fiap.balanceMe.goal.entity.Goal;
 import br.com.fiap.balanceMe.goal.mapper.GoalMapper;
 import br.com.fiap.balanceMe.goal.service.GoalService;
@@ -10,22 +12,42 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("api/v1/goal")
 public class GoalController {
-    private final GoalService goalService;
+    private final GoalService service;
+
+    @GetMapping
+    public ResponseEntity<List<GoalResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll()
+                .stream()
+                .map(GoalMapper::toGoalResponse)
+                .toList());
+    }
 
     @PostMapping
     public ResponseEntity<GoalResponse> save(@RequestBody @Valid GoalRequest request) {
         Goal newGoal = GoalMapper.toGoal(request);
-        Goal savedGoal = goalService.create(newGoal);
+        Goal savedGoal = service.create(newGoal);
         return ResponseEntity.status(HttpStatus.CREATED).body(GoalMapper.toGoalResponse(savedGoal));
+    }
+
+    @PatchMapping("{id}/complete")
+    public ResponseEntity<Void> completeGoal(@PathVariable Long id) {
+        service.completeGoal(id).orElseThrow();
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("{id}/edit")
+    public ResponseEntity<GoalUpdateResponse> edit(@PathVariable Long id, @RequestBody @Valid GoalUpdateRequest request) {
+        return service.edit(request, id)
+                .map(goal -> ResponseEntity.ok(GoalMapper.toGoalUpdateResponse(goal)))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
