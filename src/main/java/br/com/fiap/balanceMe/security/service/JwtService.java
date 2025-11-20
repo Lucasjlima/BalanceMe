39b7f27a02b2e6642e6e7497be1f7e583d3a9bde
Lucas <lucasjlima2006@gmail.com>
2025-11-20 13:53:54 -0300

    feat: adding jwt service

---
+++ src/main/java/br/com/fiap/balanceMe/security/service/JwtService.java
@@ -0,0 +1,57 @@
+package br.com.fiap.balanceMe.security.service;
+
+import br.com.fiap.balanceMe.user.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    @Value("${JWT.SECRET}")
+    private String secret;
+
+    @Value("${JWT.EXPIRATION}")
+    private Long expiration;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getUserEmail())
+                .claim("userId", user.getId())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isValid(String token, User user) {
+        return extractUsername(token).equals(user.getUserEmail()) && !isExpired(token);
+    }
+
+    private boolean isExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+}
