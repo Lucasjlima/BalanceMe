448e1a634af67332d8f4f4e154b571befcd4f06e
Lucas <lucasjlima2006@gmail.com>
2025-11-20 14:13:04 -0300

    feat: adding pre-authorize annotations

--- src/main/java/br/com/fiap/balanceMe/user/controller/AuthController.java
+++ src/main/java/br/com/fiap/balanceMe/user/controller/AuthController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,13 @@ public class AuthController {
     public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.register(UserMapper.toUser(request)));
+    }
+
+    @PostMapping("register/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<AuthResponse> registerAdmin(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.register(UserMapper.toAdmin(request)));
     }
 
     @PostMapping("/login")
