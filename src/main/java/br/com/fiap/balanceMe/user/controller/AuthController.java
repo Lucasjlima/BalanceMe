2669658ed3a5eafe89ef0a2a1719fd7fb56cfe7f
Lucas <lucasjlima2006@gmail.com>
2025-11-20 13:55:07 -0300

    feat: adding auth controller

---
+++ src/main/java/br/com/fiap/balanceMe/user/controller/AuthController.java
@@ -0,0 +1,35 @@
+package br.com.fiap.balanceMe.user.controller;
+
+import br.com.fiap.balanceMe.user.dto.request.LoginRequest;
+import br.com.fiap.balanceMe.user.dto.request.RegisterRequest;
+import br.com.fiap.balanceMe.user.dto.response.AuthResponse;
+import br.com.fiap.balanceMe.user.mapper.UserMapper;
+import br.com.fiap.balanceMe.user.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
+public class AuthController {
+    private final AuthService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.register(UserMapper.toUser(request)));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.login(request));
+    }
+
+
+}
