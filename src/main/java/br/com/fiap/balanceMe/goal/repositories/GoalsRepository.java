934e0670fe6d1856f94f0bd0abf5b0a0c4324c42
Lucas <lucasjlima2006@gmail.com>
2025-11-21 19:37:23 -0300

    feat: adding find goals by user function

--- src/main/java/br/com/fiap/balanceMe/goal/repositories/GoalsRepository.java
+++ src/main/java/br/com/fiap/balanceMe/goal/repositories/GoalsRepository.java
@@ -4,6 +4,9 @@ import br.com.fiap.balanceMe.goal.entity.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GoalsRepository extends JpaRepository<Goal, Long> {
+    List<Goal> findByUserId(Long userId);
 }
